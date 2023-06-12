b33a52aa3b45b1f49c6b92792350f6a5b26c2fe2
Mikl <maximalis171091@yandex.ru>
2023-06-12 22:36:08 +0300

    Update

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e -u
+
+# Locale
+sed -i 's/#\(ru_RU\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
+echo "LC_MESSAGES=ru_RU.UTF-8" >> /etc/locale.conf
+
+# virtual console
+echo "LOCALE=\"ru_RU.UTF-8\"" > /etc/vconsole.conf
+echo "KEYMAP=\"ru\"" >> /etc/vconsole.conf
+echo "FONT=\"cyr-sun16\"" >> /etc/vconsole.conf
+echo "CONSOLEFONT=\"cyr-sun16\"" >> /etc/vconsole.conf
+echo "USECOLOR=\"yes\"" >> /etc/vconsole.conf
+echo "TIMEZONE=\"Europe/Moscow\"" >> /etc/vconsole.conf
+echo "HARDWARECLOCK=\"UTC\"" >> /etc/vconsole.conf
+echo "CONSOLEMAP=\"\"" >> /etc/vconsole.conf
+
+# Time and clock
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc --utc
+
+# hostname
+echo "archcustom" > /etc/hostname
+
+# root and live user
+usermod -s /bin/bash root
+cp -aT /etc/skel/ /root/
+useradd -m -p "" -g users -G "adm,log,users,uucp,wheel,audio,games,disk,floppy,input,kvm,optical,scanner,storage,video,tty,utmp,network,rfkill,power" -s /bin/bash liveuser
+#chmod 750 /etc/sudoers.d
+mkdir -p /etc/sudoers.d/
+touch /etc/sudoers.d/g_wheel
+echo "root ALL=(ALL) ALL" > /etc/sudoers.d/g_wheel
+echo "%wheel  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/g_wheel
+chmod 440 /etc/sudoers.d/g_wheel
+chown -R liveuser:users /home/liveuser
+
+# Uncomment mirrors and amend journald
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+# Enable services
+systemctl enable choose-mirror.service
+systemctl set-default multi-user.target
---
+++ airootfs/skel_default/.bashrc
@@ -0,0 +1,22 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+man() {
+    env LESS_TERMCAP_mb=$'\E[01;31m' \
+    LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+    LESS_TERMCAP_me=$'\E[0m' \
+    LESS_TERMCAP_se=$'\E[0m' \
+    LESS_TERMCAP_so=$'\E[38;5;246m' \
+    LESS_TERMCAP_ue=$'\E[0m' \
+    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    man "$@"
+}
+
+alias ls='ls --color=auto'
+alias grep='grep --colour=auto'
+# PS1='[\u@\h \W]\$ '
+PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
